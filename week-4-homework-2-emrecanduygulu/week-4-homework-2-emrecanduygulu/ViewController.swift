3d90cf16807da516d2f78531e66d88f4c9ed006e
Emre Can Duygulu <ecduygulu@gmail.com>
2023-01-02 17:42:31 +0300

    initial commit

---
+++ week-4-homework-2-emrecanduygulu/week-4-homework-2-emrecanduygulu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-4-homework-2-emrecanduygulu
+//
+//  Created by emre can duygulu on 2.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
